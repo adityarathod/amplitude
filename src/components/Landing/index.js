418d7af81f74e6e180c8d0a06aad16dd52f437fc
applecrazy <applecrazy@users.noreply.github.com>
2019-02-07 09:14:22 -0800

    Update links

--- src/components/Landing/index.js
+++ src/components/Landing/index.js
@@ -10,11 +10,22 @@ const Landing = () => (
             <img src="./AmplitudeLogo.png" alt="Amplitude Logo" style={{ maxWidth: '180px' }} />
             <h1 className="title">Welcome to Amplitude</h1>
             <h3 className="subtitle">Announcements and more for multi-room events.</h3>
-            <Link to={ROUTES.LOGIN}>
-                <button className="button is-link">
-                    Login
-                    </button>
-            </Link>
+            <div className="field is-grouped" style={{ justifyContent: 'center' }}>
+                <div className="control">
+                    <Link to={ROUTES.LOGIN}>
+                        <button className="button is-link">
+                            Login
+                        </button>
+                    </Link>
+                </div>
+                <div className="control">
+                    <Link to={ROUTES.PROJECTOR}>
+                        <button className="button is-link">
+                            Projector
+                        </button>
+                    </Link>
+                </div>
+            </div>
         </div>
     </CenterScreen>
 )
